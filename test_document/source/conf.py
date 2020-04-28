3b734ac3a8922b87b465731df9dfd9a3fd34857e
RottenOrangeZZ <RottenOrangeZZ@yahoo.co.jp>
2020-04-28 12:29:24 +0900

    PyCharm commit Sphinx

--- test_document/source/conf.py
+++ test_document/source/conf.py
@@ -31,7 +31,8 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinxcontrib.seqdiag'
+    'sphinxcontrib.seqdiag',
+    'sphinx.ext.githubpages'
 ]
 
 # Fontpath for seqdiag
